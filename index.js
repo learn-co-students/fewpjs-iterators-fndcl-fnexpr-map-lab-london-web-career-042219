f8c547eebb02c073a35074f36de96866e0c217f3
aidenjl193 <aidenjl193@gmail.com>
2019-06-11 11:35:08 +0100

    Done.

--- index.js
+++ index.js
@@ -12,12 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-	tutorials.map((string) => {
-		words = string.split(" ");
-		words.map((word) => {
+	return tutorials.map((string) => {
+		const words = string.split(" ");
+		return words.map((word) => {
 			return word[0].toUpperCase() + word.slice(1);
-		});
-		return words.join(" ");
+		}).join(" ");
 	});
-  return tutorials
 }
